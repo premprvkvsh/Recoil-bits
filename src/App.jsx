f06bb46a1d91766ac62a27d39b381b008e29ee27
Prem <pprakash312003@gmail.com>
2024-08-06 00:53:12 +0530

    Async Data Queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,82 @@
 
-import './App.css'
-import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
-import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
-import { useMemo } from 'react'
+// import './App.css'
+// import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+// import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
+// import { useMemo } from 'react'
 
+
+// function App() {
+//   return <RecoilRoot>
+//     <MainApp />
+//   </RecoilRoot>
+// }
+
+// function MainApp() {
+//   const networkNotificationCount = useRecoilValue(networkAtom)
+//   const jobsAtomCount = useRecoilValue(jobsAtom);
+//   const notificationsAtomCount = useRecoilValue(notificationsAtom)
+//   const messagingAtomCount = useRecoilValue(messagingAtom)
+//   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+//   // const totalNotificationCount = useMemo(() => {
+//   //   return networkNotificationCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount;
+//   // }, [networkNotificationCount, jobsAtomCount, notificationsAtomCount, messagingAtomCount]) 
+
+//   return (
+//     <>
+//       <button>Home</button>
+      
+//       <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+//       <button>Jobs {jobsAtomCount}</button>
+//       <button>Messaging ({messagingAtomCount})</button>
+//       <button>Notifications ({notificationsAtomCount})</button>
+
+//       <button>Me ({totalNotificationCount})</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
+import { allNotificationsAtom, totalNotificationSelector } from './atoms'
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
   return <RecoilRoot>
     <MainApp />
   </RecoilRoot>
 }
 
 function MainApp() {
-  const networkNotificationCount = useRecoilValue(networkAtom)
-  const jobsAtomCount = useRecoilValue(jobsAtom);
-  const notificationsAtomCount = useRecoilValue(notificationsAtom)
-  const messagingAtomCount = useRecoilValue(messagingAtom)
-  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+  const [allNotification, setAllNotification] = useRecoilState(allNotificationsAtom);
+  const totalCount = useRecoilValue(totalNotificationSelector);
 
-  // const totalNotificationCount = useMemo(() => {
-  //   return networkNotificationCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount;
-  // }, [networkNotificationCount, jobsAtomCount, notificationsAtomCount, messagingAtomCount]) 
+  // normal way to get from server and then update to setAllNotification
+  /*
+  useEffect(() => {
+    axios.get('https://sum-server.100xdevs.com/notifications')
+      .then((res) => {
+        // console.log(res.data);
+        setAllNotification(res.data);
+      })
+  }, [])
+  */
+
+  function checkForConversion(count) {
+    return (count > 6) ? "6+" : count
+  }
 
   return (
     <>
       <button>Home</button>
-      
-      <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
-      <button>Jobs {jobsAtomCount}</button>
-      <button>Messaging ({messagingAtomCount})</button>
-      <button>Notifications ({notificationsAtomCount})</button>
-
-      <button>Me ({totalNotificationCount})</button>
+      <button>My network ({checkForConversion(allNotification.network)})</button>
+      <button>Jobs ({checkForConversion(allNotification.jobs)})</button>
+      <button>Messaging ({checkForConversion(allNotification.messaging)})</button>
+      <button>Notification ({checkForConversion(allNotification.notifications)})</button>
+      <button>Me ({totalCount})</button>
     </>
   )
 }
--- src/atoms.js
+++ src/atoms.js
@@ -1,37 +1,94 @@
+// `import {
+//     atom,
+//     selector
+// } from "recoil";
+
+// export const networkAtom = atom({
+//     key: "networkAtom",
+//     default: 102
+// });
+
+// export const jobsAtom = atom({
+//     key: "jobsAtom",
+//     default: 0
+// });
+
+// export const notificationsAtom = atom({
+//     key: "notificationsAtom",
+//     default: 12
+// });
+
+// export const messagingAtom = atom({
+//     key: "messagingAtom",
+//     default: 0
+// });
+
+// export const totalNotificationSelector = selector({
+//     key: "totalNotificationSelector",
+//     get: ({
+//         get
+//     }) => {
+//         const networkAtomCount = get(networkAtom);
+//         const jobsAtomCount = get(jobsAtom);
+//         const notificationsAtomCount = get(notificationsAtom);
+//         const messagingAtomCount = get(messagingAtom);
+//         return networkAtomCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount
+//     }
+// })
+
+import axios from "axios";
 import {
     atom,
     selector
 } from "recoil";
 
-export const networkAtom = atom({
-    key: "networkAtom",
-    default: 102
-});
+// using async data queries
+export const allNotificationsAtom = atom({
+    key: "allNotifications",
+    /*      it also work
+    default: axios.get('https://sum-server.100xdevs.com/notifications')
+    .then((res) => {
+        return res.data;
+    })
+    */
 
-export const jobsAtom = atom({
-    key: "jobsAtom",
-    default: 0
-});
-
-export const notificationsAtom = atom({
-    key: "notificationsAtom",
-    default: 12
-});
-
-export const messagingAtom = atom({
-    key: "messagingAtom",
-    default: 0
+    //  wrap the default into selector for async data queries
+    default: selector({
+        key: 'defaultSelector',
+        get: async () => {
+            const res = await axios.get('https://sum-server.100xdevs.com/notifications');
+            return res.data;
+        }
+    })
 });
 
 export const totalNotificationSelector = selector({
-    key: "totalNotificationSelector",
+    key: "totalNotifications",
     get: ({
         get
     }) => {
-        const networkAtomCount = get(networkAtom);
-        const jobsAtomCount = get(jobsAtom);
-        const notificationsAtomCount = get(notificationsAtom);
-        const messagingAtomCount = get(messagingAtom);
-        return networkAtomCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount
+        const notification = get(allNotificationsAtom);
+        return notification.network + notification.jobs + notification.messaging + notification.notifications;
     }
 })
+
+// normal approach
+/*
+export const allNotificationsAtom = atom({
+    key: "allNotifications",
+    default: {
+        network: 0,
+        jobs: 0,
+        messaging: 0,
+        notifications: 0,
+    }
+});
+
+export const totalNotificationSelector = selector({
+    key: "totalNotifications",
+    get: ({ get }) => {
+        const notification = get(allNotificationsAtom);
+        return notification.network + notification.jobs + notification.messaging + notification.notifications;
+    }
+})
+*/
